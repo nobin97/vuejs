c0440a4ce8b0a72bc1e8155ab03dd40ec19280c7
Nobin Jacob <nobinj@qburst.com>
2019-09-28 23:26:18 +0530

    improved share_everything frontend

--- main.js
+++ main.js
@@ -2,22 +2,27 @@
 // Vue.use(vueResource)
 // Vue.use(require('vue-moment'));
 
-var main = new Vue({
-  el: '#main',
+var mainapp = new Vue({
+  el: '#main-app',
   data:{
     blogs: [],
-    loading: false,
-    blog_by_id: []
+    loading: true,
+    lboard_loading: false,
+    load_error: false,
+    leaderboard: [],
+    blog_by_id: [],
+    token:'Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMmVjZGNlNWEtNzhiYy00NmViLTk4ZmMtNzUyMTRiNjhkYzA3IiwiZXhwIjoxNTY5Njk0NTM2LCJlbWFpbCI6Im5vYmluOTdAZ21haWwuY29tIiwidXNlcm5hbWUiOiJub2Jpbjk3QGdtYWlsLmNvbSJ9.ZJuLTZfYi5RXy2xt_DlnkM-6bSlrtNjgzEt2Df6Isf0'
   },
   mounted: function(){
     this.get_all_blogs();
+    this.get_leaderboard();
   },
   methods:{
     get_all_blogs: function(){
       this.loading = true;
       this.$http.get('http://localhost:8000/api/v1/share/blogs/', {
         headers: {
-          'Authorization': 'Token eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiOGNjZGFhZWQtNWM5Yy00YmNmLTg4MDMtNmU4YjliM2EwOWRhIiwidXNlcm5hbWUiOiJub2JpbmpAcWJ1cnN0LmNvbSIsImV4cCI6MTU2OTU5Mjk3MiwiZW1haWwiOiJub2JpbmpAcWJ1cnN0LmNvbSJ9.rNYYARF2Iyca9Awt16y3vhP4GMhByKHt7RhbKoGXF0w'
+          'Authorization': this.token
         }
       })
       .then((response) => {
@@ -26,6 +31,7 @@ var main = new Vue({
       })
       .catch((err) => {
         this.loading = false;
+        this.load_error = true;
         console.log(err);
       })
     },
@@ -34,25 +40,87 @@ var main = new Vue({
       this.loading = true;
       this.$http.get(('http://localhost:8000/api/v1/share/blogs/'+ id + '/'), {
         headers: {
-          'Authorization': 'Token eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiOGNjZGFhZWQtNWM5Yy00YmNmLTg4MDMtNmU4YjliM2EwOWRhIiwidXNlcm5hbWUiOiJub2JpbmpAcWJ1cnN0LmNvbSIsImV4cCI6MTU2OTU5Mjk3MiwiZW1haWwiOiJub2JpbmpAcWJ1cnN0LmNvbSJ9.rNYYARF2Iyca9Awt16y3vhP4GMhByKHt7RhbKoGXF0w'
+          'Authorization': this.token
         }
       })
       .then((response) => {
-        this.blog_by_id = response.data;
-        console.log(this.blog_by_id)
+        this.upvote = response.status;
         this.loading = false;
+        this.get_all_blogs();
       })
       .catch((err) => {
         this.loading = false;
         console.log("some error");
         console.log(err);
       })
+    },
+    up_vote : function(id){
+      this.$http.get(('http://localhost:8000/api/v1/share/blogs/'+ id + '/upvote/'), {
+        headers: {
+          'Authorization': this.token
+        }
+      })
+      .then((response) => {
+        console.log(this.blog_by_id)
+        this.loading = false;
+        this.get_all_blogs();
+        this.get_leaderboard();
+      })
+      .catch((err) => {
+        this.loading = false;
+        console.log("some error");
+        console.log(err);
+      })
+    },
+    get_leaderboard: function(){
+      this.lboard_loading = true;
+      this.$http.get('http://localhost:8000/api/v1/share/blogs/leaderboard/', {
+        headers: {
+          'Authorization': this.token
+        }
+      })
+      .then((response) => {
+        this.leaderboard = response.data;
+        console.log(this.leaderboard)
+        this.lboard_loading = false;
+      })
+      .catch((err) => {
+        this.lboard_loading = false;
+        this.load_error = true;
+        console.log(err);
+      })
     }
-  },
-  computed: {
-      rows() {
-        return this.blogs.length
-      }
-    }
+  }
 })
-//
+
+// var leaderboard = new Vue({
+//   el:"#leaderboard",
+//   data:{
+//     leaderboard: [],
+//     load_error: false,
+//     loading: false,
+//     token:'Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMmVjZGNlNWEtNzhiYy00NmViLTk4ZmMtNzUyMTRiNjhkYzA3IiwiZXhwIjoxNTY5Njk0NTM2LCJlbWFpbCI6Im5vYmluOTdAZ21haWwuY29tIiwidXNlcm5hbWUiOiJub2Jpbjk3QGdtYWlsLmNvbSJ9.ZJuLTZfYi5RXy2xt_DlnkM-6bSlrtNjgzEt2Df6Isf0'
+//   },
+//   mounted: function() {
+//     this.get_leaderboard();
+//   },
+//   methods:{
+//     get_leaderboard: function(){
+//       this.loading = true;
+//       this.$http.get('http://localhost:8000/api/v1/share/blogs/leaderboard/', {
+//         headers: {
+//           'Authorization': this.token
+//         }
+//       })
+//       .then((response) => {
+//         this.leaderboard = response.data;
+//         this.loading = false;
+//       })
+//       .catch((err) => {
+//         this.loading = false;
+//         this.load_error = true;
+//         console.log(err);
+//       })
+//     }
+//   }
+// })
